1e03be5813f7acd4186e2c70fe7b8abe0075deda
Egor Rya <fayncore@gmail.com>
2021-02-07 19:04:42 +0300

    Log in firebase

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,29 @@
-import react from 'react';
-import AppRouter from './Router';
+import react, { useState, useEffect } from "react";
+import AppRouter from "./Router";
+import { authService } from "../firebase";
 
 function App() {
-  return <AppRouter/>;
+  const [init, setInit] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  // waititng for firebase to login
+  useEffect(() => {
+    authService.onAuthStateChanged((user) => {
+      if (user) {
+        setIsLoggedIn(true);
+      } else {
+        setIsLoggedIn(false);
+      }
+      setInit(true);
+    });
+  }, []);
+
+  return (
+    <>
+      {init ? <AppRouter isLoggedIn={isLoggedIn} /> : "Initializing"}
+      <footer>&copy; {new Date().getFullYear()} Notwitter</footer>
+    </>
+  );
 }
 
 export default App;
